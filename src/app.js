55c54ec60cdd32bf9a5877c205005e82515e77b2
arat-arjat <anderaniel@gmail.com>
2023-04-04 22:04:45 +0000

    proyecto culminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// const { NoEmitOnErrorsPlugin } = require("webpack");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".us", ".io"];
+
+pronoun.forEach(word => {
+  adj.forEach(adjetive => {
+    noun.forEach(nouns => {
+      domain.forEach(domains => {
+        console.log(word + adjetive + nouns + domains);
+      });
+    });
+  });
+});
